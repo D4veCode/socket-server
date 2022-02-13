d4b5c118c53b24851ac72e55786bcc4f271b295e
David <davidasb.27@gmail.com>
2022-02-12 22:39:31 -0400

    refactor: :recycle: added ws to listen a petition

--- src/controllers/get.controller.js
+++ src/controllers/get.controller.js
@@ -2,6 +2,9 @@ import socket  from '../socket';
 let socketServer = socket.socket
 
 const sendMessage = () => {
+  socketServer.io.on('message1', (data) => {
+    console.log('data');
+  })
   socketServer.io.emit('message', 'HOLA')
   return { message: 'HOLA' }
 }
