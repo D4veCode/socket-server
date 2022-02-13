7060bdff3281d8dac20f97a2febe0edeb7ccb208
David <davidasb.27@gmail.com>
2022-02-12 23:06:12 -0400

    fix: :bug: fixed cors errors

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,7 @@ import {Server} from 'socket.io';
 let socket = {}
 
 const connect = (server) => {
-  socket.io = new Server(server)
+  socket.io = new Server(server, {cors: {origin: 'http://localhost:3000', methods: ["GET", "POST", "PUT", "DELETE"]}});
 }
 
 export default {
